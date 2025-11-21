cb1652e548029ea7928e184c7f2bc3440ec4d237
vunraks <mihalovi.kom@gmail.com>
2025-11-21 18:26:22 +0500

    Lesson_02 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Bb(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Товар',
+    )
+    content = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name = 'Описание',
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Цена',
+
+    )
+    published = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Опубликовано',
+    )
+
+    def __str__(self):
+        return f'Объявление: {self.title}'
+
+    class Meta:
+        verbose_name='Объявление'
+        verbose_name_plural='Объявления'
+        ordering = ('-published',)
--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from bboard.models import Bb
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    # template = loader.get_template('index.html')
+    bbs = Bb.objects.all()
+    context = {'bbs': bbs}
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
+
+
