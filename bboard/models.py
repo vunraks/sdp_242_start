199fd4974739a5b74face073c8b7ed07f9e0b717
vunraks <mihalovi.kom@gmail.com>
2025-11-24 18:42:43 +0500

    lesson_03 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        verbose_name='Название',
+    )
+
+    def __str__(self):
+        return f'{ self.name}'
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ('name',)
+
+
 class Bb(models.Model):
     title = models.CharField(
         max_length=50,
@@ -20,6 +36,14 @@ class Bb(models.Model):
         auto_now_add=True,
         db_index=True,
         verbose_name='Опубликовано',
+    )
+
+    rubric = models.ForeignKey(
+        'Rubric',
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика'
     )
 
     def __str__(self):
--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from bboard.models import Bb
+from django.views.generic import CreateView
+
+from bboard.forms import BbForm
+from bboard.models import Bb, Rubric
 
 
 # Create your views here.
@@ -9,9 +12,28 @@ from bboard.models import Bb
 def index(request):
     # template = loader.get_template('index.html')
     bbs = Bb.objects.all()
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
 
     # return HttpResponse(template.render(context, request))
     return render(request, 'index.html', context)
 
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
